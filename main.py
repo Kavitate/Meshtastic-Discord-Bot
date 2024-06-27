a914c7fa693bae8b159ffe28413692971aae0424
Kavitate <79380289+Kavitate@users.noreply.github.com>
2024-06-27 10:18:05 -0500

    Update main.py
    Added longname to received messages.
    Added timestamps to sent and received messages.
    Refactored the /active command.

--- main.py
+++ main.py
@@ -49,14 +49,18 @@ meshtodiscord = queue.Queue()
 discordtomesh = queue.Queue()
 nodelistq = queue.Queue()
 
-def onConnectionMesh(interface, topic=pub.AUTO_TOPIC): #called when we (re)connect to the meshtastic radio
+def onConnectionMesh(interface, topic=pub.AUTO_TOPIC): # Called when reconnecting to the Meshtastic radio
     print(interface.myInfo)
 
-def onReceiveMesh(packet, interface): #called when a packet arrives from mesh
-    print("onReceiveMesh called")  # Debugging print statement
+def get_long_name(node_id, nodes):
+    if node_id in nodes:
+        return nodes[node_id]['user'].get('longName', 'Unknown')
+    return 'Unknown'
+
+def onReceiveMesh(packet, interface): # Called when a packet arrives from mesh
     try: 
-        if packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP': #only interest in text packets for now
-            print("Text message packet received")  # Debugging print statement
+        if packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP':
+            print("Text message packet received")  # For debugging
             print(f"Packet: {packet}")  # Print the entire packet for debugging
 
             # Check if 'channel' is present in the top-level packet
@@ -68,25 +72,31 @@ def onReceiveMesh(packet, interface): #called when a packet arrives from mesh
                     channel_index = packet['decoded']['channel']
                 else:
                     channel_index = 0  # Default to channel 0 if not present
-                    print("Channel not found in packet, defaulting to channel 0")  #Debugging print statement
+                    print("Channel not found in packet, defaulting to channel 0")  # For debugging
 
             channel_name = channel_names.get(channel_index, f"Unknown Channel ({channel_index})")
 
+            current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
+            nodes = interface.nodes
+            from_long_name = get_long_name(packet['fromId'], nodes)
+            to_long_name = get_long_name(packet['toId'], nodes) if packet['toId'] != '^all' else 'All Nodes'
+
             embed = discord.Embed(title="Message Received", description=packet['decoded']['text'], color=0x67ea94)
-            embed.add_field(name="From Node", value=packet['fromId'], inline=True)
+            embed.add_field(name="From Node", value=f"{from_long_name} ({packet['fromId']})", inline=True)
+            embed.set_footer(text=f"{current_time}")
 
             if packet['toId'] == '^all':
                 embed.add_field(name="To Channel", value=channel_name, inline=True)
             else:
-                embed.add_field(name="To Node", value=packet['toId'], inline=True)
+                embed.add_field(name="To Node", value=f"{to_long_name} ({packet['toId']})", inline=True)
 
             meshtodiscord.put(embed)
 
-    except KeyError as e: #catch empty packet
-        print(f"KeyError: {e}")  # Print the error for debugging
+    except KeyError as e: # Catch empty packet
         pass
     except Exception as e:  # Catch any other exceptions
-        print(f"Unexpected error: {e}")  # Print the error for debugging
+        print(f"Unexpected error: {e}")  # For debugging
         pass
 
 class MeshBot(discord.Client):
@@ -95,8 +105,7 @@ class MeshBot(discord.Client):
         self.tree = app_commands.CommandTree(self)
         self.iface = None  # Initialize iface as None
 
-    async def setup_hook(self) -> None:
-        # create the background task and run it in the background
+    async def setup_hook(self) -> None: # Create the background task and run it in the background
         self.bg_task = self.loop.create_task(self.background_task())
         await self.tree.sync()
 
@@ -117,14 +126,12 @@ class MeshBot(discord.Client):
             sys.exit(1)
         while not self.is_closed():
             counter += 1
-            if (counter % 12 == 1):
-                # approx 1 minute (every 12th call, call every 5 seconds), refresh node list
-                nodelist = ["Nodes seen in the last 15 minutes:\n"]
+            if (counter % 12 == 1): # Approx 1 minute (every 12th call, call every 5 seconds), refresh node list
+                nodelist = ["**Nodes seen in the last 15 minutes:**\n"]
                 nodes = self.iface.nodes
                 for node in nodes:
                     try:
                         id = str(nodes[node]['user']['id'])
-                        num = str(nodes[node]['user']['num'])
                         longname = str(nodes[node]['user']['longName'])
                         if "hopsAway" in nodes[node]:
                             hopsaway = str(nodes[node]['hopsAway'])
@@ -136,16 +143,15 @@ class MeshBot(discord.Client):
                             snr = "?"
                         if "lastHeard" in nodes[node]:
                             ts = int(nodes[node]['lastHeard'])
-                            # Convert UTC timestamp to eastern time
-                            eastern_tz = pytz.timezone('YOUR-TZ')
-                            eastern_time = datetime.fromtimestamp(ts, tz=pytz.utc).astimezone(eastern_tz)
-                            timestr = eastern_time.strftime('%d %B %Y %I:%M:%S %p')
+                            time_zone = pytz.timezone('YOUR-TZ')
+                            current_time = datetime.fromtimestamp(ts, tz=pytz.utc).astimezone(time_zone)
+                            timestr = current_time.strftime('%d %B %Y %I:%M:%S %p')
                         else:
                             # 15 minute time for active nodes
                             ts = time.time() - (16 * 60)
                             timestr = "Unknown"
                         if ts > time.time() - (15 * 60):
-                            nodelist.append(f"\n**ID:** {id} | **Number:** {num} | **Long Name:** {longname} | **Hops:** {hopsaway} | **SNR:** {snr} | **Last Heard:** {timestr}")
+                            nodelist.append(f"\n**ID:** {id} | **Long Name:** {longname} | **Hops:** {hopsaway} | **SNR:** {snr} | **Last Heard:** {timestr}")
                     except KeyError as e:
                         print(e)
                         pass
@@ -178,7 +184,7 @@ class MeshBot(discord.Client):
                 pass
             try:
                 nodelistq.get_nowait()
-                # if there's any item on this queue, we'll send the nodelist
+                # If there are any item on this queue it sends the nodelist
                 for chunk in nodelist_chunks:
                     await channel.send(chunk)
                 nodelistq.task_done()
@@ -203,26 +209,23 @@ async def help_command(interaction: discord.Interaction):
     await interaction.response.defer(ephemeral=False)
 
     help_text = ("**Command List**\n"
-                 "`/sendid` - Send a message to another node.\n"
-                 "`/sendnum` - Send a message to another node.\n"
-                 "`/active` - Shows all active nodes.\n"
-                 "`/channel0` to send a message in channel 0.\n"
-                 "`/channel1` to send a message in channel 1.\n"
-                 "`/channel2` to send a message in channel 2.\n"
-                 "`/channel3` to send a message in channel 3.\n"
-                 "`/channel4` to send a message in channel 4.\n"
-                 "`/channel5` to send a message in channel 5.\n"
-                 "`/channel6` to send a message in channel 6.\n"
-                 "`/channel7` to send a message in channel 7.\n"
-                 "`/active` - Shows all active nodes.\n"
-                 "`/help` - Shows this help message.\n\n")
+         "`/sendid` - Send a message to another node.\n"
+         "`/sendnum` - Send a message to another node.\n"
+         "`/active` - Shows all active nodes.\n"
+         "`/channelname0` to send a message in channel 0.\n"
+         "`/channelname1` to send a message in channel 1.\n"
+         "`/channelname2` to send a message in channel 2.\n"
+         "`/channelname3` to send a message in channel 3.\n"
+         "`/channelname4` to send a message in channel 4.\n"
+         "`/channelname5` to send a message in channel 5.\n"
+         "`/channelname6` to send a message in channel 6.\n"
+         "`/channelname7` to send a message in channel 7.\n"
+         "`/help` - Shows this help message.\n\n")
 
     color = 0x67ea94
 
     embed = discord.Embed(title="Meshtastic Bot Help", description=help_text, color=color)
     embed.set_footer(text="Meshtastic Discord Bot by Kavitate")
-
-    # Use the image from the URL
     ascii_art_image_url = "https://i.imgur.com/qvo2NkW.jpeg"
     embed.set_image(url=ascii_art_image_url)
 
@@ -239,8 +242,11 @@ async def sendid(interaction: discord.Interaction, nodeid: str, message: str):
         # Convert hexadecimal node ID to decimal
         nodenum = int(nodeid, 16)
 
+        current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
         embed = discord.Embed(title="Sending Message", description=message, color=0x67ea94)
         embed.add_field(name="To Node:", value=f"!{nodeid}", inline=True)  # Add '!' in front of nodeid
+        embed.set_footer(text=f"{current_time}")
         await interaction.response.send_message(embed=embed, ephemeral=False)
         discordtomesh.put(f"nodenum={nodenum} {message}")
     except ValueError:
@@ -249,56 +255,92 @@ async def sendid(interaction: discord.Interaction, nodeid: str, message: str):
 
 @client.tree.command(name="sendnum", description="Send a message to a specific node.")
 async def sendnum(interaction: discord.Interaction, nodenum: int, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message", description=message, color=0x67ea94)
     embed.add_field(name="To Node:", value=str(nodenum), inline=True)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"nodenum={nodenum} {message}")
 
 @client.tree.command(name="CHANNEL-NAME-0", description="Send a message in CHANNEL-NAME-0.")
 async def channelname0(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-0:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=0 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-1", description="Send a message in CHANNEL-NAME-1.")
 async def channelname1(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-1:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=1 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-2", description="Send a message in CHANNEL-NAME-2.")
 async def channelname2(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-2:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=2 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-3", description="Send a message in CHANNEL-NAME-3.")
 async def channelname3(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-3:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=3 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-4", description="Send a message in CHANNEL-NAME-4.")
 async def channelname4(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-4:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=4 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-5", description="Send a message in CHANNEL-NAME-5.")
 async def channelname5(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-5:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=5 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-6", description="Send a message in CHANNEL-NAME-6.")
 async def channelname6(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-6:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=6 {message}")
 
 @client.tree.command(name="CHANNEL-NAME-7", description="Send a message in CHANNEL-NAME-7.")
 async def channelname7(interaction: discord.Interaction, message: str):
+
+    current_time = datetime.now().strftime('%d %B %Y %I:%M:%S %p')
+
     embed = discord.Embed(title="Sending Message to CHANNEL-NAME-7:", description=message, color=0x67ea94)
+    embed.set_footer(text=f"{current_time}")
     await interaction.response.send_message(embed=embed)
     discordtomesh.put(f"channel=7 {message}")
 
@@ -306,49 +348,9 @@ async def channelname7(interaction: discord.Interaction, message: str):
 async def active(interaction: discord.Interaction):
     await interaction.response.defer()
 
-    nodelist = ["**Nodes seen in the last 15 minutes:**\n"]
-    nodes = client.iface.nodes
-    for node in nodes:
-        try:
-            id = str(nodes[node]['user']['id'])
-            num = str(nodes[node]['num'])
-            longname = str(nodes[node]['user']['longName'])
-            if "hopsAway" in nodes[node]:
-                hopsaway = str(nodes[node]['hopsAway'])
-            else:
-                hopsaway = "0"
-            if "snr" in nodes[node]:
-                snr = str(nodes[node]['snr'])
-            else:
-                snr = "?"
-            if "lastHeard" in nodes[node]:
-                ts = int(nodes[node]['lastHeard'])
-                eastern_tz = pytz.timezone('YOUR-TZ')
-                eastern_time = datetime.fromtimestamp(ts, tz=pytz.utc).astimezone(eastern_tz)
-                timestr = eastern_time.strftime('%d %B %Y %I:%M:%S %p')
-            else:
-                ts = time.time() - (16 * 60)
-                timestr = "Unknown"
-            if ts > time.time() - (15 * 60):
-                nodelist.append(f"\n**ID:** {id} | **Number:** {num} | **Long Name:** {longname} | **Hops:** {hopsaway} | **SNR:** {snr} | **Last Heard:** {timestr}")
-        except KeyError as e:
-            print(e)
-            pass
-
-    # Check if nodelist contains only the header
-    if len(nodelist) == 1:
-        await interaction.followup.send("No active nodes seen in the last 15 minutes.", ephemeral=True)
-    else:
-        # Split nodelist into chunks of 10 rows
-        nodelist_chunks = ["".join(nodelist[i:i+10]) for i in range(0, len(nodelist), 10)]
-
-        # Send the first chunk as a follow-up message
-        await interaction.followup.send(nodelist_chunks[0])
-
-        # Send the remaining chunks as normal messages
-        channel = interaction.channel
-        for chunk in nodelist_chunks[1:]:
-            await channel.send(chunk)
+    nodelistq.put(True)
+    
+    await asyncio.sleep(1)
 
 def run_discord_bot():
     try:
